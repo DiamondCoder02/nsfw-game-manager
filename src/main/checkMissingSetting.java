6865647620915049969f1b38b560e60b5fb99498
DiamondPRO02 <kovacsdanielmark@gmail.com>
2023-06-22 01:09:33 +0200

    Working settings check

--- src/main/checkMissingSetting.java
+++ src/main/checkMissingSetting.java
@@ -12,7 +12,6 @@ import org.w3c.dom.Document;
 
 public class checkMissingSetting {
 	public static void checkSettings() {
-		// TODO check settings and update if missing
 		String[] settings = {"othersettings", "language", "showncolumns"};
 		String[] othersettings = {"Dark mode", "Auto fetch game info"};
 		String[] language = {"english"};
@@ -22,39 +21,34 @@ public class checkMissingSetting {
 			"Last update", "People rating", "Player progress", 
 			"Still on pc?", "Engine", "OS", "Personal Notes"
 		};
-		try{
-			Boolean otSe, laSe, shCo;
-			Document dom = xmlFolderHandle.saveLoadDoc.loadDocument(mainInit.settingsPath);
-			if (dom == null) {
-				createMissingSettings.createFile(mainInit.settingsPath);
-			} else {
-				try{
-					NodeList settingSource = dom.getElementsByTagName("settings");
-					if (settingSource.getLength() == 0) { deleteThenCreateBroken(); }
-					Node settingNode = settingSource.item(0);
-					if (settingNode.getNodeType() == Node.ELEMENT_NODE) {
-						Element e = (Element) settingNode;
-						for (int i = 0; i < settings.length; i++) {
-							NodeList setting = e.getElementsByTagName(settings[i]);
-							System.out.println("Checking " + settings[i] + "...");
-							switch (settings[i]) {
-								case "othersettings": otSe = checkings(setting, othersettings); break;
-								case "language": laSe = checkings(setting, language); break;
-								case "showncolumns": shCo = checkings(setting, showncolumns); break;
-								default: JOptionPane.showMessageDialog(null, "Should be impossible checkMissingSetting", "Error", JOptionPane.ERROR_MESSAGE); break;
-							}
+		Boolean otSe = false, laSe = false, shCo = false;
+		Document dom = xmlFolderHandle.saveLoadDoc.loadDocument(mainInit.settingsPath);
+		if (dom == null) {
+			createMissingSettings.createFile(mainInit.settingsPath);
+		} else {
+			try{
+				NodeList settingSource = dom.getElementsByTagName("settings");
+				if (settingSource.getLength() == 0) { deleteThenCreateBroken(); }
+				Node settingNode = settingSource.item(0);
+				if (settingNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element e = (Element) settingNode;
+					for (int i = 0; i < settings.length; i++) {
+						NodeList setting = e.getElementsByTagName(settings[i]);
+						switch (settings[i]) {
+							case "othersettings": otSe = checkings(setting, othersettings, dom, "othersettings"); break;
+							case "language": laSe = checkings(setting, language, dom, "language"); break;
+							case "showncolumns": shCo = checkings(setting, showncolumns, dom, "showncolumns"); break;
+							default: JOptionPane.showMessageDialog(null, "Should be impossible checkMissingSetting", "Error", JOptionPane.ERROR_MESSAGE); break;
 						}
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
-					JOptionPane.showMessageDialog(null, "Error checking settings. (checkMissingSetting.checkSettings_2)", "Error", JOptionPane.ERROR_MESSAGE);
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				JOptionPane.showMessageDialog(null, "Error checking settings. (checkMissingSetting.checkSettings)", "Error", JOptionPane.ERROR_MESSAGE);
 			}
-			// TODO show update dialog
-			System.out.println("Done checking settings.");
-		} catch (Exception e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Error checking settings. (checkMissingSetting.checkSettings)", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		if (otSe || laSe || shCo) {
+			JOptionPane.showMessageDialog(null, "Settings got updated. \nPlease restart the program to make sure everything is correct.", "Settings updated", JOptionPane.INFORMATION_MESSAGE);
 		}
 	}
 
@@ -69,19 +63,22 @@ public class checkMissingSetting {
 		}
 	}
 
-	private static Boolean checkings(NodeList setting, String[] somethingSettings){
+	private static Boolean checkings(NodeList setting, String[] somethingSettings, Document dom, String settingName){
 		Boolean settingsGotUpdated = false;
 		for (int j = 0; j < somethingSettings.length; j++) {
 			String[] toCheckWith = new String[somethingSettings.length];
 			for (int k = 0; k < setting.getLength(); k++) { toCheckWith[k] = setting.item(k).getTextContent(); }
 			for (int k = 0; k < toCheckWith.length; k++) { if (toCheckWith[k] == null) { toCheckWith[k] = "-"; } }
-			System.out.println(toCheckWith[j] + " -> "+somethingSettings[j]);
-			if (toCheckWith[j].contains(somethingSettings[j])) {
-				System.out.println("! " + somethingSettings[j]);
-			} else {
-				System.out.println("? " + somethingSettings[j]);
+			if (!toCheckWith[j].contains(somethingSettings[j])) {
+				Element newSetting = dom.createElement(settingName);
+				if (somethingSettings[j] == "Auto fetch game info") { newSetting.setAttribute("enabled", "false");
+				} else { newSetting.setAttribute("enabled", "true"); }
+				newSetting.appendChild(dom.createTextNode(somethingSettings[j]));
+				dom.getElementsByTagName("settings").item(0).appendChild(newSetting);
+				settingsGotUpdated = true;
 			}
 		}
+		xmlFolderHandle.saveLoadDoc.saveDocument(dom, mainInit.settingsPath);
 		return settingsGotUpdated;
 	}
 }

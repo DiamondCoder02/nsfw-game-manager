34a545375fbbc2245cfca24e37172c5f289791bc
DiamondCoder <femboyowo.supp@gmail.com>
2023-08-15 19:11:03 +0200

    update faq and credit

--- src/main/application/otherButtons.java
+++ src/main/application/otherButtons.java
@@ -51,15 +51,17 @@ public class otherButtons {
 		"- Add, store, update, remove game infos manually"+br+
 		"- DarkMode"+br+
 		"- Colorful table"+br+
-		"- F95zone, so only ID needed to add, remove game"+br+
-		"-----*Plans:*-----"+br+
 		"- Search function"+br+
 		"- Sorting"+br+
+		"- Random game chooser"+br+
+		"- After selecting a folder it will check for games and automatically update table( details on Github)"+br+
+		"- F95zone, so only ID needed to add, update, remove game"+br+
+		"-----*Plans:*-----"+br+
 		"- Dlsite so only ID needed"+br+
-		"- (Far future) If I can, I will also add to download/detect what games are downloaded"+br+br+
+		"- (Far future) If I can, I will also add to download / autoupdate games"+br+br+
 		"Q: Support the project?"+br+"A: Patreon:"+" <font color = 64AFFF><a href=\"https://www.patreon.com/DiamondCoder\">https://www.patreon.com/DiamondCoder</a></font>"+br+
 		"or on Github:"+" <font color = 64AFFF><a href=\"https://github.com/sponsors/DiamondPRO02\">https://github.com/sponsors/DiamondPRO02</a></font>"+br+br+
-		"Q: Can I help code / Error in the program?"+br+"A: All isssue and help is accepted on github:"+" <font color = 64AFFF><a href=\"https://github.com/DiamondPRO02/nsfw-game-manager\">https://github.com/DiamondPRO02/nsfw-game-manager</a></font>"+br+br+
+		"Q: Can I help code / Error in the program?"+br+"A: All isssue and help is accepted on GitHub:"+" <font color = 64AFFF><a href=\"https://github.com/DiamondPRO02/nsfw-game-manager\">https://github.com/DiamondPRO02/nsfw-game-manager</a></font>"+br+br+
 		"Q: placeholder?"+br+"A: placeholder!"+
 		"</span></p>");
 		ep.addHyperlinkListener(new HyperlinkListener() {
@@ -96,13 +98,13 @@ public class otherButtons {
 		Font font = new Font("Arial", Font.PLAIN, 20);
 		ep.setFont(font);
 		ep.setText("<p style=\"font-family: Arial\"><span style=\"color:"+color+"\">"+(btn[7]!=null?btn[7]:"Credits")+br+br+
-		"Hi, I'm Diamond."+br+
-		"This is a small project after learning Java in university."+br+
-		"The main of this was to convert my old excel file into something more readeble."+br+
+		"Hi, I'm DiamondCoder or Diamond."+br+
+		"This is a small project for learning Java in university."+br+
+		"The main reason for this was to convert my old excel file into something more readeble and more usefull."+br+
 		"Main focus is something dynamic and easy to use for hentai games."+br+br+
-		"Thank you for using this program."+br+br+
+		"Thank you for using this program, it gives me smile I can make something good."+br+br+
 		"Full open source:"+" <font color = 64AFFF><a href=\"https://github.com/DiamondPRO02/nsfw-game-manager\">https://github.com/DiamondPRO02/nsfw-game-manager</a></font>"+br+
-		"Support me on Github or on Patreon:"+" <font color = 64AFFF><a href=\"https://www.patreon.com/DiamondCoder\">https://github.com/DiamondPRO02/nsfw-game-manager</a></font></span></p>"+br);
+		"Support me on Github or on Patreon:"+" <font color = 64AFFF><a href=\"https://www.patreon.com/DiamondCoder\">https://www.patreon.com/DiamondCoder</a></font></span></p>"+br);
 		ep.addHyperlinkListener(new HyperlinkListener() {
 			@Override
 			public void hyperlinkUpdate(HyperlinkEvent e) {

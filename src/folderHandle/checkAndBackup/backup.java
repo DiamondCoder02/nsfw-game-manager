2d56dfe09c1e1a6a52b9cda0e72fe67d3abd8cda
DiamondCoder <diamond.coder.backup@gmail.com>
2023-09-08 12:28:29 +0200

    Fixed backup

--- src/folderHandle/checkAndBackup/backup.java
+++ src/folderHandle/checkAndBackup/backup.java
@@ -17,15 +17,6 @@ public class backup {
 		if (!new File(backupPath).exists()) {
 			new File(backupPath).mkdirs();
 		}
-		// create backup file with date and time
-		String date = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
-		String backupFile = backupPath+"/backup_"+date+".xml";
-		// copy main file to backup file
-		try {
-			Files.copy(new File(mainDataPath).toPath(), new File(backupFile).toPath());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
 		// delete files except the latest 10
 		File[] files = new File(backupPath).listFiles();
@@ -34,11 +25,21 @@ public class backup {
 				return Long.valueOf(f2.lastModified()).compareTo(f1.lastModified());
 			}
 		});
-		int count = files.length - 10;
+		int count = files.length - 9;
 		if (count<0) { count = 0; }
 		for (int i=0; i<count; i++) {
 			// System.out.println("Deleted: " + files[i].getName());
 			files[i].delete();
+		}
+
+		// create backup file with date and time
+		String date = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+		String backupFile = backupPath+"/backup_"+date+".xml";
+		// copy main file to backup file
+		try {
+			Files.copy(new File(mainDataPath).toPath(), new File(backupFile).toPath());
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
